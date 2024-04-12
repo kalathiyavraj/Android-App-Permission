1fc7b7c4296d1175a552a639f1e54e32edcf3b06
kalathiya vraj <84270801+kalathiyavraj@users.noreply.github.com>
2024-04-12 10:43:37 +0530

    Add files via upload

---
+++ PermissionActivity.java
@@ -0,0 +1,74 @@
+package photocreation.camera.blurcamera.App_Permission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.InstallStatus;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import photocreation.camera.blurcamera.Activity.HomeActivity;
+import photocreation.camera.blurcamera.R;
+import photocreation.camera.blurcamera.Utils.Tools;
+
+public class PermissionActivity extends AppCompatActivity {
+    LinearLayout btn_permission;
+    public PermissionClass permissionClass = new PermissionClass(this);
+    private FirebaseAnalytics mFirebaseAnalytics;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Tools.setSystemBarColor(this, 17170443);
+        Tools.setSystemBarLight(this);
+
+        Bundle bundle1 = new Bundle();
+        bundle1.putString("PermissionActivity","1");
+        photocreation.camera.blurcamera.Utils.Utils.mFirebaseAnalytics.logEvent("PermissionActivity", bundle1);
+
+
+
+        btn_permission = findViewById(R.id.start_permission);
+
+
+
+
+        btn_permission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PermissionClass permissionClass = PermissionActivity.this.permissionClass;
+                PermissionClass.RequestPermissions();
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int i, String[] strArr, int[] iArr) {
+        super.onRequestPermissionsResult(i, strArr, iArr);
+        if (PermissionClass.HasPermission(PermissionActivity.this)) {
+
+            Intent intent = new Intent(PermissionActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        } else if (!PermissionClass.checkRequestPermissionRationale()) {
+            PermissionClass.openSettingDialog();
+        }
+    }
+}
---
+++ PermissionClass.java
@@ -0,0 +1,63 @@
+package photocreation.camera.blurcamera.App_Permission;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class PermissionClass {
+    public static String[] all_permissions = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.CAMERA"};
+    public static String[] all_permissions_33 = {"android.permission.CAMERA", "android.permission.READ_MEDIA_IMAGES", "android.permission.READ_MEDIA_VIDEO","android.permission.POST_NOTIFICATIONS"};
+    public static Activity mActivity;
+
+    public PermissionClass(Activity activity) {
+        mActivity = activity;
+    }
+
+    public static String[] permissions() {
+        if (Build.VERSION.SDK_INT >= 33) {
+            return all_permissions_33;
+        }
+        return all_permissions;
+    }
+
+    public static void RequestPermissions() {
+        try {
+            ActivityCompat.requestPermissions(mActivity, permissions(), 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean HasPermission(Context context) {
+        if (Build.VERSION.SDK_INT >= 33) {
+            if (ContextCompat.checkSelfPermission(context, "android.permission.CAMERA") == 0 && ContextCompat.checkSelfPermission(context, "android.permission.READ_MEDIA_IMAGES") == 0 && ContextCompat.checkSelfPermission(context, "android.permission.READ_MEDIA_VIDEO") == 0) {
+                return true;
+            }
+        } else if (ContextCompat.checkSelfPermission(context, "android.permission.WRITE_EXTERNAL_STORAGE") == 0 && ContextCompat.checkSelfPermission(context, "android.permission.READ_EXTERNAL_STORAGE") == 0 && ContextCompat.checkSelfPermission(context, "android.permission.CAMERA") == 0) {
+            return true;
+        }
+        return false;
+    }
+    public static boolean checkRequestPermissionRationale() {
+        if (Build.VERSION.SDK_INT >= 33) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(mActivity, "android.permission.CAMERA") && !ActivityCompat.shouldShowRequestPermissionRationale(mActivity, "android.permission.READ_MEDIA_IMAGES") && !ActivityCompat.shouldShowRequestPermissionRationale(mActivity, "android.permission.READ_MEDIA_VIDEO")) {
+                return false;
+            }
+        } else if (!ActivityCompat.shouldShowRequestPermissionRationale(mActivity, "android.permission.CAMERA") && !ActivityCompat.shouldShowRequestPermissionRationale(mActivity, "android.permission.READ_EXTERNAL_STORAGE") && !ActivityCompat.shouldShowRequestPermissionRationale(mActivity, "android.permission.WRITE_EXTERNAL_STORAGE")) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void openSettingDialog() {
+        Intent intent = new Intent("android.settings.APPLICATION_DETAILS_SETTINGS");
+        intent.addFlags(268435456);
+        intent.setData(Uri.fromParts("package", PermissionClass.mActivity.getPackageName(), null));
+        PermissionClass.mActivity.startActivity(intent);
+    }
+}
